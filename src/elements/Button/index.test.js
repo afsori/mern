805f581973dbd459fbf3e3b77b6ab9eb1374b035
afsori <afsoryafsor@gmail.com>
2020-05-31 10:06:29 +0700

    initial commit

---
+++ src/elements/Button/index.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { render } from "@testing-library/react";
+import Button from "./index.js";
+
+test("Should not allow click if disabled", () => {
+  const { container } = render(<Button isDisabled></Button>);
+
+  expect(container.querySelector("span.disabled")).toBeInTheDocument();
+});
+
+test("Should render loading/spinner", () => {
+  const { container, getByText } = render(<Button isLoading></Button>);
+
+  expect(getByText(/loading/i)).toBeInTheDocument();
+
+  expect(container.querySelector("span")).toBeInTheDocument();
+});
+
+test("Should render a tag", () => {
+  const { container } = render(<Button type="link" isExternal></Button>);
+
+  expect(container.querySelector("a")).toBeInTheDocument();
+});
+
+test("Should render <Link> component", () => {
+  const { container } = render(
+    <Router>
+      <Button href="" type="link"></Button>
+    </Router>
+  );
+
+  expect(container.querySelector(`a`)).toBeInTheDocument();
+});
---
+++ src/pages/LandingPages.js
@@ -0,0 +1,6 @@
+import React from "react";
+// import module from "elements/Button";
+
+export default function LandingPages() {
+  return <div></div>;
+}
